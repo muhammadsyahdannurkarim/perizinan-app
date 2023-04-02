a9659d7a13f96701b5bdb015c80c61de778cb6d8
aaalamandaardana <ardana1920@gmail.com>
2023-04-02 14:12:53 +0700

    update template lagi

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,6 +1,6 @@
 @extends('Layouts.main')
 
-@section('title', 'Kelola Izin - ')
+@section('title', 'Kontak - ')
 
 @section('container')
 
@@ -12,7 +12,7 @@
         <div class="container position-relative">
           <div class="row d-flex justify-content-center">
             <div class="col-lg-6 text-center">
-              <h2>Blog</h2>
+              <h2>Kontak</h2>
               <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
             </div>
           </div>
